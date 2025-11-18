8b821fbebc1b689d613a82c25550e2722d13def8
soramame0925 <sora1968924391014@gmail.com>
2025-11-18 09:01:07 +0900

    Delete m-no-yakata directory

--- m-no-yakata/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="mno-footer">
-  <p>&copy; <?php echo date('Y'); ?> ドMの館</p>
-</footer>
-
-<?php get_template_part('parts/fix-nav'); ?>
-
-<?php wp_footer(); ?>
-</body>
-</html>
--- m-no-yakata/index.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-<main class="mno-container">
-  <h1>テスト表示中：M-no-Yakata テーマ</h1>
-  <p>このテーマが正常に有効化されていれば、ここが表示されます。</p>
-</main>
-
-<?php get_footer(); ?>
